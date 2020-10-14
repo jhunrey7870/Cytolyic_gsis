cc3d7d4ee0a26aaa3cec74147ce7c79ded43455c
aktham3210 <71159394+aktham3210@users.noreply.github.com>
2020-10-15 01:55:28 +0200

    Update pizza.sh

--- Generic/pizza.sh
+++ Generic/pizza.sh
@@ -4,8 +4,8 @@ sudo ./ErfanGSIs/url2GSI.sh $ROM_URL Generic
     mkdir final
     sudo chmod -R 777 ErfanGSIs/output
     cd ErfanGSIs/output
-    zip -r $ROM_NAME-CytolyticGSIs-Aonly.7z *-Aonly-*.img
-    zip -r $ROM_NAME-CytolyticGSIs-AB.7z *-AB-*.img
+    zip -r $ZIP_NAME-CytolyticGSIs-Aonly.7z *-Aonly-*.img
+    zip -r $ZIP_NAME-CytolyticGSIs-AB.7z *-AB-*.img
     curl -sL https://git.io/file-transfer | sh
     echo "::set-env name=arm64ab::$(./transfer $MIR $ROM_NAME-CytolyticGSIs-Aonly.7z" | grep -o -P '(?<=Download Link: )\S+')" 
     echo "::set-env name=arm64a-only::$(./transfer $MIR $ROM_NAME-CytolyticGSIs-AB.7z" | grep -o -P '(?<=Download Link: )\S+')"

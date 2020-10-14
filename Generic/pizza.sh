e578779406104a35fe0260c0d16d65afd5ea0911
aktham3210 <71159394+aktham3210@users.noreply.github.com>
2020-10-15 01:23:25 +0200

    I'm an idiot

--- Generic/pizza.sh
+++ Generic/pizza.sh
@@ -5,7 +5,7 @@ sudo ./ErfanGSIs/url2GSI.sh $ROM_URL Generic
     sudo chmod -R 777 ErfanGSIs/output
     cd ErfanGSIs/output
     zip -r $ROM_NAME-CytolyticGSIs-Aonly.7z *-Aonly-*.img
-    ZIP -r $ROM_NAME-CytolyticGSIs-AB.7z
+    zip -r $ROM_NAME-CytolyticGSIs-AB.7z
     curl -sL https://git.io/file-transfer | sh
     echo "::set-env name=arm64ab::$(./transfer $MIR $ZIP_NAME-CytolyticGSIs-Aonly.7z" | grep -o -P '(?<=Download Link: )\S+')" 
     echo "::set-env name=arm64a-only::$(./transfer $MIR $ROM_NAME-CytolyticGSIs-AB.7z" | grep -o -P '(?<=Download Link: )\S+')"

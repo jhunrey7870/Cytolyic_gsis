c87d08f0a39afe6b0b74ee9469fe5a2bd65e4dd2
aktham3210 <71159394+aktham3210@users.noreply.github.com>
2020-10-15 02:36:38 +0200

    Update pizza.sh

--- Generic/pizza.sh
+++ Generic/pizza.sh
@@ -6,8 +6,8 @@ sudo ./ErfanGSIs/url2GSI.sh $ROM_URL Generic
     cd ErfanGSIs/output
     telegram -m "test LOL"
     telegram -m "still testing"
-    zip -r $ROM-AB-$sourcever2-$date2-ErfanGSI-cytolytic.zip *-AB-*.img
-    zip -r $ROM-Aonly-$sourcever2-$date2-ErfanGSI-cytolytic.zip *-A-only-*.img
+    zip -r $ROM_NAME-AB-$sourcever2-$date2-ErfanGSI-cytolytic.zip *-AB-*.img
+    zip -r $ROM_NAME-Aonly-$sourcever2-$date2-ErfanGSI-cytolytic.zip *-A-only-*.img
     curl -sL https://git.io/file-transfer | sh
-    echo "::set-env name=DOWNLOAD_A::$(./transfer $MIR "$ROM-Aonly-$sourcever2-$date2-ErfanGSI-cytolytic.zip" | grep -o -P '(?<=Download Link: )\S+')"
-    echo "::set-env name=DOWNLOAD_AB::$(./transfer $MIR "$ROM-AB-$sourcever2-$date2-ErfanGSI-cytolytic.zip" | grep -o -P '(?<=Download Link: )\S+')"
+    echo "::set-env name=DOWNLOAD_A::$(./transfer $MIR "$ROM_NAME-Aonly-$sourcever2-$date2-ErfanGSI-cytolytic.zip" | grep -o -P '(?<=Download Link: )\S+')"
+    echo "::set-env name=DOWNLOAD_AB::$(./transfer $MIR "$ROM_NAME-AB-$sourcever2-$date2-ErfanGSI-cytolytic.zip" | grep -o -P '(?<=Download Link: )\S+')"
